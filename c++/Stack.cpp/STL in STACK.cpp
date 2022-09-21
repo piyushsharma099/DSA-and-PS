869111c801c3451af42ba2aef8950e070def922b
Piyush Sharma <2205piyush@gmail.com>
2022-09-21 19:56:54 +0530

    .vscode added

---
+++ c++/Stack.cpp/STL in STACK.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    stack<int>s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.push(6);
+    cout<<s.top()<<" ";
+}
