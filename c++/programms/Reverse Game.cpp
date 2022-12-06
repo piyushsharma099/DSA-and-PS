4b4c309599a1f83dfebe0d09ce231a96a5bb237b
piyushh <2205piyush@gmail.com>
2022-12-06 16:56:45 +0530

    added

--- c++/programms/Reverse Game.cpp
+++ c++/programms/Reverse Game.cpp
@@ -1,35 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-int reverse(int arr[], int start, int end){
-    while (start < end){
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++;
-        end--;
-    }
-    return 0;
-} 
-
 int main() {
   int T;
   cin >> T;
   while(T--){
     int N, K,ans;
-    cin >> N >> K;
+    cin >>N>>K;
     int a[N];
     for(int i=0 ; i<N ; i++){
         a[i]=i;
     }
-    for(int i=0 ; i<N ; i++){
-        reverse(a,i,N);
+    int b[N];
+    int s=0;
+    int e=N-1;
+    int k=0;
+    while(k<N){
+        b[k]=a[e];
+        b[k+1]=a[s];
+        s++;
+        e--;
+        k+=2;
     }
     for(int i=0 ; i<N ; i++){
-        cout<<a[i];
+        cout<<b[i];
     }
     cout<<endl;
     // for(int i=0 ; i<N ; i++){
-    //     if(a[i]==K){
+    //     if(b[i]==K){
     //         ans=i;
     //         break;
     //     }
